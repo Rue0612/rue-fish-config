e28ae931da80b32453ecdfcef10327211abb337f
Rue Coimbra <69476182+Rue0612@users.noreply.github.com>
2026-04-18 22:34:09 -0300

    Cleared fish historic on exit

--- config.fish
+++ config.fish
@@ -5,3 +5,8 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #function fish_greeting
 #    # smth smth
 #end
+
+function __clear_history_on_exit --on-event fish_exit
+    history clear
+    rm -f ~/.local/share/fish/fish_history
+end
